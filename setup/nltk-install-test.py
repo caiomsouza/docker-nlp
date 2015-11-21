7b4d28b332af72b2038492f6ec2e906d0771c775
Caio Moreno de Souza <caiomsouza@gmail.com>
2015-11-21 21:36:13 +0100

    test

--- setup/nltk-install-test.py
+++ setup/nltk-install-test.py
@@ -1,4 +1,5 @@
 import nltk
 from nltk.corpus import brown
-brown.words()
-print "NLTK and NTLK All Data were installed successfully."
+print "We will now the nltk.corpus to see if it will works..."
+print brown.words()
+print "If you see the words: [u'The', u'Fulton', u'County', u'Grand', u'Jury', ...] above then it means that  NLTK and NTLK All Data were installed successfully."
