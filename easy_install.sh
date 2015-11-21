a840478dfdb1f513e1eb3d4e179743b7e0f6bdf6
Caio Moreno de Souza <caiomsouza@gmail.com>
2015-11-21 21:17:33 +0100

    melhorias

--- easy_install.sh
+++ easy_install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-OPT=$1
-INSTALL_DIR=`pwd`
-PDI_BUILD=${INSTALL_DIR}/pdi
-BISERVER_BUILD=${INSTALL_DIR}/biserver
-RUN_STACK="docker-compose up -d"
 
+#OPT=$1
+#INSTALL_DIR=`pwd`
+#PDI_BUILD=${INSTALL_DIR}/pdi
+#BISERVER_BUILD=${INSTALL_DIR}/biserver
+#RUN_STACK="docker-compose up -d"
 
 
 # Docker Installer
@@ -12,10 +12,14 @@ sudo wget https://raw.githubusercontent.com/it4biz/ubuntu-docker-installer/maste
 sudo sh ubuntu-docker-installer.sh
 
 #NLTK Installer
+sudo sh setup/nltk-installer.sh
+
+#NLTK ALL Data Installer
+sh setup/nltk-installer-all-data.sh
 
 
-cd ${PDI_BUILD} && sh ${PDI_BUILD}/build.sh
-cd ${BISERVER_BUILD} && sh ${BISERVER_BUILD}/build.sh
-cd ${INSTALL_DIR}
+#cd ${PDI_BUILD} && sh ${PDI_BUILD}/build.sh
+#cd ${BISERVER_BUILD} && sh ${BISERVER_BUILD}/build.sh
+#cd ${INSTALL_DIR}
 
 
